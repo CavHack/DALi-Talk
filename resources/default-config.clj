b6d2a6241e5dbc74fe792d80c33db26c589fbe74
CavHack <karl@dali.ninja>
2016-03-22 01:31:49 -0400

    The Yelp API gets INPUT from DALi-Talk - A Semantic GET amd POST

---
+++ resources/default-config.clj
@@ -0,0 +1,33 @@
+{
+ :fr$core {:sagan ["fr.time"
+ 	  	   "fr.numbers"
+		   "fr.temperature"
+		   "fr.measure"
+		   "fr.finances"
+		   "en.communication"
+ 	  	    ]
+           :rules ["fr.time"
+	           "fr.numbers"
+		   "fr.cycles"
+		   "fr.duration"
+		   "fr.temperature"
+		   "fr.measure"
+		   "en.finance"
+		   "en.communication"
+		   ]}
+   :en$core {:sagan ["en.time"
+   	             "en.numbers"
+		     "en.temperature"
+		     "en.measure"
+		     "en.finance"
+		     "en.communication"] 
+             
+	     :rules ["en.time"
+	             "en.numbers"
+		     "en.cycles"
+		     "en.duration"
+		     "en.temperature"
+		     "en.measure"
+		     "en.finance"
+		     "en.communication"]}
+}
